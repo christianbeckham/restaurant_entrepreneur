276f7b9c15affcb3f21739cfd31dbd0401bf5264
Christian Beckham <urenacb22@gmail.com>
2022-09-12 14:03:46 -0500

    feat: added logic to run_simulation method

--- simulation.py
+++ simulation.py
@@ -1,6 +1,15 @@
+from franchise import Franchise
+
+
 class Simulation:
     def __init__(self):
         pass
 
     def run_simulation(self):
-        pass
+        franchise_one = Franchise(1)
+        franchise_two = Franchise(2)
+        franchise_three = Franchise(3)
+
+        franchise_one.place_order()
+        franchise_two.place_order()
+        franchise_three.place_order()
